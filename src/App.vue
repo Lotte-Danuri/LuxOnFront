ccf5f10daa6d4d773ecdb81e3b89d5517491e4e8
이충현 <a8207637@naver.com>
2022-10-24 17:37:50 +0900

    FEAT: UI 변경

--- src/App.vue
+++ src/App.vue
@@ -18,24 +18,14 @@ function buttonClick() {
   <TheView></TheView>
 
   <TheChat
-    style="position: fixed; z-index: 999; right: 50px; bottom: 50px"
+    style="position: fixed; z-index: 999; right: 30px; bottom: 30px"
     @click="buttonClick"
   ></TheChat>
   <Transition>
     <TheChatApp
-      class="border border-3 rounded"
+      @closeBtn="buttonClick"
       v-if="chatShow"
-      style="
-        position: fixed;
-        width: 30%;
-        height: 60%;
-        right: 130px;
-        bottom: 130px;
-        z-index: 999;
-        background-color: grey;
-        min-height: 800px;
-        min-width: 600px;
-      "
+      style="position: fixed; right: 30px; bottom: 30px; z-index: 999"
     ></TheChatApp>
   </Transition>
 </template>
--- src/layouts/TheChatApp.vue
+++ src/layouts/TheChatApp.vue
@@ -1,62 +1,122 @@
 <template>
-  <div class="chat-app-wrapper">
-    <div class="chat-app-header border">
-      <div class="logo">
-        <img
-          class="chat-msg-img"
-          src="@/assets/logo/logo_white.png"
-          alt=""
-          width="100px"
-          height="100%"
-        />
-      </div>
-      <button class="btn close-btn">
-        <i class="bi bi-x-circle"></i>
+  <div class="chat_app_wrapper">
+    <div class="chat_app_header border" v-if="showChat">
+      <button class="btn" @click="backButton">
+        <i class="bi bi-chevron-left"></i>
+      </button>
+      <p class="app__brand">이름 오는곳</p>
+      <button class="btn" @click="$emit('closeBtn')">
+        <i class="bi bi-x-lg"></i>
       </button>
     </div>
-    <div class="chat-app-body border">
-      <div class="chat-app-sidebar"></div>
-      <div class="content"></div>
+    <div v-if="!showChat" class="chat_app_body border">
+      <div
+        v_for="conversation in props.conversations"
+        :key="conversation.id"
+        class="chat_room_list"
+        :class="[props.conversationId === conversation.id && 'active']"
+        tabindex="0"
+        @keydown.space.prevent="
+          () => {
+            emit('update:conversationId', conversation.id);
+            emit('toggleMobileConversation');
+          }
+        "
+        @click="
+          () => {
+            emit('update:conversationId', conversation.id);
+            emit('toggleMobileConversation');
+          }
+        "
+      ></div>
     </div>
-    <div class="chat-app-footer border"></div>
+    <chat-list v-if="showChat"></chat-list>
+    <ChatForm @submitMessage="sendMessage" v-if="showChat"></ChatForm>
   </div>
 </template>
-<script></script>
-<style>
-.chat-app-wrapper {
+<script>
+export default {
+  data: function () {
+    return {
+      showChat: true,
+    };
+  },
+};
+function backButton() {
+  this.showChat = !this.showChat;
+}
+</script>
+<style scoped>
+@import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css');
+@font-face {
+  font-family: 'Golden Plains - Demo';
+  src: url('./assets/GoldenPlains.woff2') format('woff2'),
+    url('./assets/GoldenPlains.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
+  font-display: swap;
+}
+* {
+  font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
+    'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR',
+    'Malgun Gothic', sans-serif;
+}
+body {
+  background-color: #cfcff4;
+}
+.app__brand {
   display: flex;
   flex-direction: column;
+  align-items: center;
 }
-.chat-app-header {
-  background-color: red;
+.chat_app_wrapper {
+  width: 375px;
+  height: 812px;
+  background-color: #ffffff;
+  margin: 5rem auto 0rem;
+  border-radius: 1.5rem;
+  box-shadow: 0px 1px 20px #9c9cc855;
   display: flex;
-  width: 100%;
-  height: 80px;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.chat_app_header {
+  background: #ffffff;
+  box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.05);
+  border-radius: 24px 24px 0px 0px;
+  font-size: 16px;
+  font-weight: 700;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  height: 70px;
+}
+.chat__header__greetings {
+  color: #292929;
 }
 
-.chat-app-body {
-  flex-grow: 1;
-  background-color: white;
+.chat_room_list {
   width: 100%;
-  display: flex;
+  display: list_item;
 }
-
-.chat-app-sidebar {
+.chat_app_sidebar {
+  display: none;
   background-color: yellow;
   width: 20%;
 }
 
 .content {
+  display: none;
   background-color: aqua;
   width: 80%;
 }
-.chat-app-footer {
+.chat_app_footer {
   background-color: black;
   width: 100%;
   height: 80px;
 }
 
-.close-btn {
-  right: 10px;
+.close_btn {
+  justify-content: flex_end;
 }
 </style>
