6f13571c997863d3af9cd3b26166230eb7b76749
이충현 <a8207637@naver.com>
2022-10-25 16:43:27 +0900

    Revert "Merge branch 'develop' into FEAN-176-chat-system-front-end-개발"
    This reverts commit 3ebcf520619ae985f9b9eb23bed5a8ca30a5cc76, reversing
    changes made to 54f42f5e8235de4a8940aecbb9b0a248c22b6f06.

--- src/views/admin/DashBoard.vue
+++ src/views/admin/DashBoard.vue
@@ -1,24 +1,4 @@
-<template>
-  <div class="dashContainer">
-    <aside>
-      <div class="top">
-        <div class="logo">
-          <img src="@/assets/logo/logo_black.png" />
-          <h2>EGA<span class="danger">TOR</span></h2>
-        </div>
-        <div class="close" id="close-btn">
-          <span class="material-icons-sharp">close</span>
-        </div>
-      </div>
-      <div class="siderbar">
-        <a href="#">
-          <span class="material-icons-sharp">grid_view</span>
-          <h3>Dashboard</h3>
-        </a>
-      </div>
-    </aside>
-  </div>
-</template>
+<template>dashboard</template>
 <script>
 export default {
   components: {},
