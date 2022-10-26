3da45dee24eb4eb0a2953876609eae1f1aa65ad1
YJay94 <83999253+YJay94@users.noreply.github.com>
2022-10-26 10:11:24 +0900

    (FEAN-140) temporary commit

--- src/layouts/admin/adminIndex.vue
+++ src/layouts/admin/adminIndex.vue
@@ -404,4 +404,310 @@ main .recent-orders a {
   margin: 1rem auto;
   color: var(--color-primary);
 }
+
+/* ============== RIGHT ================ */
+.right {
+  margin-top: 1.4rem;
+}
+
+.right .top {
+  display: flex;
+  justify-content: end;
+  gap: 2rem;
+  background: gray;
+}
+
+.right .top button {
+  display: none;
+}
+
+.right .theme-toggler {
+  background: var(--color-light);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 1.6rem;
+  width: 4.2rem;
+  cursor: pointer;
+  border-radius: var(--border-radius-1);
+}
+
+.right .theme-toggler span {
+  font-size: 1.2rem;
+  width: 50%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.right .theme-toggler span.active {
+  background: var(--color-primary);
+  color: white;
+  border-radius: var(--border-radius-1);
+}
+
+.right .top .profile {
+  display: flex;
+  gap: 2rem;
+  text-align: right;
+}
+
+/* ================ RECENT UPDATES =========== */
+.right .recent-updates {
+  margin-top: 1rem;
+}
+
+.right .recent-updates h2 {
+  margin-bottom: 0.8rem;
+}
+
+.right .recent-updates .update {
+  background: var(--color-white);
+  padding: var(--card-padding);
+  border-radius: var(--card-border-radius);
+  box-shadow: var(--box-shadow);
+  transition: all 300ms ease;
+}
+
+.right .recent-updates .updates:hover {
+  box-shadow: none;
+}
+
+.right .recent-updates .updates .update {
+  display: grid;
+  grid-template-columns: 2.6rem auto;
+  gap: 1rem;
+  margin-bottom: 1rem;
+}
+
+/* ================= SALES ANALYTICS ============= */
+.right .sales-anlytics {
+  margin-top: 2rem;
+}
+
+.right .sales-analytics h2 {
+  margin-bottom: 0.8rem;
+}
+
+.right .sales-analytics .item {
+  background: var(--color-white);
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-bottom: 0.7rem;
+  padding: 1.4rem var(--card-padding);
+  border-radius: var(--border-radius-3);
+  box-shadow: var(--box-shadow);
+  transition: all 300ms ease;
+}
+
+.right .sales-analytics .item:hover {
+  box-shadow: none;
+}
+
+.right .sales-analytics .item .right {
+  display: flex;
+  justify-content: space-between;
+  align-items: start;
+  margin: 0;
+  width: 100%;
+}
+
+.right .sales-analytics .item .icon {
+  padding: 0.6rem;
+  color: var(--color-white);
+  border-radius: 50%;
+  background: red;
+  display: flex;
+}
+
+.right .sales-analytics .item.offline .icon {
+  background: var(--color-success);
+}
+
+.right .sales-analytics .item.customers .icon {
+  background: var(--color-danger);
+}
+
+.right .sales-anlytics .add-product {
+  background-color: transparent;
+  border: 2px dashed var(--color-primary);
+  color: var(--color-primary);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.right .sales-analytics .add-product div {
+  display: flex;
+  align-items: center;
+  gap: 0.6rem;
+}
+
+.right .sales-analytics .add-product div h3 {
+  font-weight: 600;
+}
+
+/* ================== MEDIA QUERIES ============= */
+@media screen and (max-width: 1200px) {
+  .dashContainer {
+    width: 94%;
+    grid-template-columns: 7rem auto 23rem;
+  }
+
+  aside .logo h2 {
+    display: none;
+  }
+
+  aside .sidebar h3 {
+    display: none;
+  }
+
+  aside .sidebar a {
+    width: 5.6rem;
+  }
+
+  aside .siderbar a:last-child {
+    position: relative;
+    margin-top: 1.8rem;
+  }
+
+  main .insights {
+    grid-template-columns: 1fr;
+    gap: 0;
+  }
+
+  main .recent-orders {
+    width: 94%;
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    margin: 2rem 0 0 8.8rem;
+  }
+
+  main .recent-orders table {
+    width: 83vw;
+  }
+
+  main table thead tr th:last-child,
+  main table thead tr th:first-child {
+    display: none;
+  }
+
+  main table tbody tr th:last-child,
+  main table tbody tr th:first-child {
+    display: none;
+  }
+}
+
+/* ====================== MEDIA QUERIES =============== */
+@media screen and (max-width: 768px) {
+  .dashContainer {
+    width: 100%;
+    grid-template-columns: 1fr;
+  }
+
+  aside {
+    position: fixed;
+    left: 0;
+    background: var(--color-white);
+    width: 18rem;
+    z-index: 3;
+    box-shadow: 1rem 3rem 4rem var(--color-light);
+    height: 100vh;
+    padding-right: var(--card-padding);
+  }
+
+  @keyframes showMenu {
+    to {
+      left: 0;
+    }
+  }
+
+  aside .logo {
+    margin-left: 1rem;
+  }
+
+  aside .logo h2 {
+    display: inline;
+  }
+
+  aside .sidebar h3 {
+    display: inline;
+  }
+
+  aside .sidebar a {
+    width: 100%;
+    height: 3.4rem;
+  }
+
+  aside .sidebar a:last-child {
+    position: absolute;
+    bottom: 5rem;
+  }
+
+  aside .close {
+    display: inline-block;
+    cursor: pointer;
+  }
+
+  main {
+    margin-top: 8rem;
+    padding: 0 1rem;
+  }
+
+  main .recent-orders {
+    position: relative;
+    margin: 3rem 0 0 0;
+    width: 100%;
+  }
+
+  main .recent-orders table {
+    width: 100%;
+    margin: 0;
+  }
+
+  .right {
+    width: 94%;
+    margin: 0 auto 4rem;
+  }
+
+  .right .top {
+    position: fixed;
+    top: 0;
+    left: 0;
+    align-items: center;
+    padding: 0 0.8rem;
+    height: 4.6rem;
+    background: var(--color-white);
+    width: 100%;
+    margin: 0;
+    z-index: 2;
+    box-shadow: 0 1rem 1rem var(--color-light);
+  }
+
+  .right .top .theme-toggler {
+    width: 4.4rem;
+    position: absolute;
+    left: 66%;
+  }
+
+  .right .profile .info {
+    display: none;
+  }
+
+  .right .top button {
+    display: inline-block;
+    background: transparent;
+    cursor: pointer;
+    color: var(--color-dark);
+    position: absolute;
+    left: 1rem;
+  }
+
+  .right .top button span {
+    font-size: 2rem;
+  }
+}
 </style>
--- src/views/admin/DashBoard.vue
+++ src/views/admin/DashBoard.vue
@@ -3,8 +3,11 @@
     <aside>
       <div class="top">
         <div class="logo">
-          <img src="@/assets/logo/logo_black.png" />
-          <h2 class="text-muted">EGA<span class="danger">TOR</span></h2>
+          <img
+            src="@/assets/logo/logo_black.png"
+            style="width: 100px; height: 100px"
+          />
+          <h2 class="text-muted">Lux<span class="danger">ON</span></h2>
         </div>
         <div class="close" id="close-btn">
           <span class="material-icons-sharp">close</span>
@@ -146,7 +149,7 @@
           <span class="material-icons-sharp"> menu </span>
         </button>
         <div class="theme-toggler">
-          <span class="material-icons-sharp">light_mode</span>
+          <span class="material-icons-sharp active">light_mode</span>
           <span class="material-icons-sharp">dark_mode</span>
         </div>
         <div class="profile">
@@ -154,12 +157,98 @@
             <p>Hey, <b>Daniel</b></p>
             <small class="text-muted">Admin</small>
           </div>
+          <div class="profile-photo">
+            <img src="./images/profile-1.jpg" />
+          </div>
+        </div>
+      </div>
+      <!-- END OF TOP -->
+      <h2>Recent Updates</h2>
+      <div class="updates">
+        <div class="date">
+          <div class="profile-photo">
+            <img src="./images/profile-2.jpg" />
+          </div>
+          <div class="message">
+            <p>
+              <b>Mike Tyson</b> received his order or Night lion tech GPS drone.
+            </p>
+            <small class="text-muted">2 Minutes Ago</small>
+          </div>
+        </div>
+      </div>
+      <div class="updates">
+        <div class="date">
+          <div class="profile-photo">
+            <img src="./images/profile-3.jpg" />
+          </div>
+          <div class="message">
+            <p>
+              <b>Mike Tyson</b> received his order or Night lion tech GPS drone.
+            </p>
+            <small class="text-muted">2 Minutes Ago</small>
+          </div>
+        </div>
+      </div>
+      <div class="updates">
+        <div class="date">
+          <div class="profile-photo">
+            <img src="./images/profile-4.jpg" />
+          </div>
+          <div class="message">
+            <p>
+              <b>Mike Tyson</b> received his order or Night lion tech GPS drone.
+            </p>
+            <small class="text-muted">2 Minutes Ago</small>
+          </div>
+        </div>
+      </div>
+      <!------------------------- END OF RECENT UPDATES ------------------------------->
+      <div class="sales-analytics">
+        <h2>Sales Analytics</h2>
+        <div class="item offline">
+          <div class="icon">
+            <span class="material-icons-sharp"> local_mall </span>
+          </div>
+          <div class="right">
+            <div class="info">
+              <h3>OFFLINE ORDERS</h3>
+              <small class="text-muted">Last 24 Hours</small>
+            </div>
+            <h5 class="danger">-17%</h5>
+            <h5>1100</h5>
+          </div>
+        </div>
+        <div class="item online">
+          <div class="icon">
+            <span class="material-icons-sharp"> shopping_cart </span>
+          </div>
+          <div class="right">
+            <div class="info">
+              <h3>ONLINE ORDERS</h3>
+              <small class="text-muted">Last 24 Hours</small>
+            </div>
+            <h5 class="success">+25%</h5>
+            <h5>849</h5>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script>
+const sideMenu = document.querySelector('aside');
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+
+menuBtn.addEventListener('click', () => {
+  sideMenu.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+  sideMenu.style.display = 'none';
+});
+
 export default {
   components: {},
   data() {
