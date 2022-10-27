642c13db60db011150078c75b40b455c021ff908
YJay94 <83999253+YJay94@users.noreply.github.com>
2022-10-27 17:46:02 +0900

    (FEAN-140) Main페이지 swiper 추가

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Index from '@/layouts/luxon/index.vue';
 import MyMain from '@/views/luxon/myMain.vue';
 import MyList from '@/views/luxon/myList.vue';
+import MyProduct from '@/views/luxon/product/myProduct.vue';
 import MyAdmin from '@/layouts/admin/adminIndex.vue';
 import DashBoard from '@/views/admin/dashBoard.vue';
 import MyLogin from '@/views/luxon/auth/myLogin.vue';
@@ -24,6 +25,11 @@ const routes = [
         path: '/list',
         name: 'MyList',
         component: MyList,
+      },
+      {
+        path: '/product/myProduct',
+        name: 'MyProduct',
+        component: MyProduct,
       },
       {
         path: '/login',
