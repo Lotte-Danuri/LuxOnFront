1854935a217210fad5e8af72f23538e227b40cbf
jinwooooooo <54875278+jinwooooooo@users.noreply.github.com>
2022-11-02 15:11:15 +0900

    (FEAN-140) feat : 쿠폰등록

--- src/views/admin/regiCoupon.vue
+++ src/views/admin/regiCoupon.vue
@@ -2,21 +2,21 @@
   <h1>쿠폰 등록</h1>
   <main>
     <div class="top_div">
-      <input placeholder="쿠폰명" />
-      <input placeholder="내용" />
-      <input placeholder="할인율" />
+      <input placeholder="쿠폰명" id="couponName"/>
+      <input placeholder="내용" id="couponContent"/>
+      <input placeholder="할인율" id="discountRate"/>
       <br />
-      <input type="date" placeholder="시작일" />
-      <input type="date" placeholder="종료일" />
+      <input type="datetime-local" placeholder="시작일" id="startDate"/>
+      <input type="datetime-local" placeholder="종료일" id="endDate"/>
       <br />
-      <input placeholder="최소주문금액" />
-      <input placeholder="최대할인금액" />
+      <input placeholder="최소주문금액" id="minOrderPrice"/>
+      <input placeholder="최대할인금액" id="maxDiscountPrice"/>
       <br />
       <input placeholder="상품선택" />
       <br />
 
-      <select @change="changeCategoryFirst($event)" id="first_select">
-        <option>첫번째 카테고리를 선택해주세요</option>
+      <select @change="changeCategoryFirst($event)" id="first_category_id">
+        <option :value="-1">-- 전체 -- (첫번째 카테고리)</option>
         <option v-for="(category, i) in categoryList" :key="i" :value="i">
           <p>{{ category.categoryName }}</p>
           <div :id="`${category.id}`" style="dispaly: none"></div>
@@ -24,26 +24,18 @@
       </select>
       <br />
       <br />
-      <select @change="changeCategorySecond($event)" id="second_select">
-        <option>두번째 카테고리를 선택해주세요</option>
-        <option
-          v-for="(categorySecond, i) in categorySecondList"
-          :key="i"
-          :value="i"
-        >
+      <select @change="changeCategorySecond($event)">
+        <option :value="-1">-- 전체 -- (두번째 카테고리)</option>
+        <option v-for="(categorySecond, i) in categorySecondList" :key="i" :value="i">
           <p>{{ categorySecond.categoryName }}</p>
           <p :id="`${categorySecond.id}`" style="dispaly: none"></p>
         </option>
       </select>
       <br />
       <br />
-      <select id="third_select" @change="changeCategoryThird()">
-        <option>세번째 카테고리를 선택해주세요</option>
-        <option
-          v-for="(categoryThird, i) in categoryThirdList"
-          :key="i"
-          :value="i"
-        >
+      <select @change="changeCategoryThird($event)">
+        <option :value="-1">-- 전체 -- (세번째 카테고리)</option>
+        <option v-for="(categoryThird, i) in categoryThirdList" :key="i" :value="i">
           <p>{{ categoryThird.categoryName }}</p>
           <p :id="`${categoryThird.id}`" style="dispaly: none"></p>
         </option>
@@ -52,98 +44,6 @@
       <br />
     </div>
 
-    <!-- <div class="category_grid" style="display: none">
-      <div>
-        <h3>대분류</h3>
-        <div v-for="(category, i) in categoryList" :key="i" :value="i">
-          <input
-            type="checkbox"
-            :id="`${category.id}`"
-            name="vehicle1"
-            :value="`${category.id}`"
-            v-model="firstNames"
-            class="myCheckbox"
-            @click="calc"
-          />
-          <label :for="`label_${category.id}`">
-            {{ category.categoryName }}</label
-          >
-        </div>
-        <br />
-      </div>
-      <div>
-        <h3>중분류</h3>
-        <div
-          v-for="(categorySecond, i) in categorySecondList"
-          :key="i"
-          :value="i"
-        >
-          <input
-            type="checkbox"
-            :id="`${categorySecond.id}`"
-            name="vehicle1"
-            :value="`${categorySecond.id}`"
-            v-model="secondNames"
-            class="myCheckbox"
-          />
-          <label :for="`label_${categorySecond.id}`">
-            {{ categorySecond.categoryName }}</label
-          ><br />
-        </div>
-      </div>
-      <div>
-        <h3>소분류</h3>
-        <div
-          v-for="(categoryThird, i) in categoryThirdList"
-          :key="i"
-          :value="i"
-        >
-          <input
-            type="checkbox"
-            :id="`${categoryThird.id}`"
-            name="vehicle1"
-            :value="`${categoryThird.id}`"
-            v-model="thirdNames"
-            class="myCheckbox"
-          />
-          <label :for="`label_${categoryThird.id}`">{{
-            categoryThird.categoryName
-          }}</label
-          ><br />
-        </div>
-      </div>
-      <span>대분류값 : {{ firstNames }}</span>
-      <span>중분류값 : {{ seconddNames }}</span>
-      <span>소분류값 : {{ thirdNames }}</span>
-    </div> -->
-
-    <table id="customers">
-      <tr>
-        <th>상품선택</th>
-        <th>이미지</th>
-        <th>상품명</th>
-        <th>상품가격</th>
-        <th>재고</th>
-        <th>보증기간</th>
-        <th>게시일</th>
-      </tr>
-      <tr v-for="product in productList" :key="i">
-        <td><input type="checkbox" /></td>
-        <td>
-          <img :src="`${product.thumbnailUrl}`" />
-        </td>
-        <td>{{ product.productName }}</td>
-        <td>{{ product.price }}</td>
-        <td>{{ product.stock }}</td>
-        <td>{{ product.warranty }}</td>
-        <td>{{ product.createdDate }}</td>
-      </tr>
-    </table>
-    <button
-      style="background-color: black; width: 100px; height: 50px; color: white"
-    >
-      등록
-    </button>
     <button
       style="
         background-color: white;
@@ -153,9 +53,76 @@
         border: solid 1px black;
         margin-left: 100px;
       "
-      @click="testClick"
+      @click="productSearch"
     >
-      test
+      상품검색
+    </button>
+
+    <table id="customers">
+      <tr>
+        <th>
+          <td><input type="checkbox"/></td>
+        </th>
+        <th>이미지</th>
+        <th>상품명</th>
+        <th>상품가격</th>
+        <th>재고</th>
+        <th>보증기간</th>
+        <th>게시일</th>
+      </tr>
+      <tr v-for="product in productList">
+        <td><input type="checkbox" v-model="productCheckVmodel" :value="product"/></td>
+        <td>
+          <img :src="`${product.thumbnailUrl}`" style="height: 100px;"/>
+        </td>
+        <td>{{ product.productName }}</td>
+        <td>{{ product.price }}</td>
+        <td>{{ product.stock }}</td>
+        <td>{{ product.warranty }}</td>
+        <td>{{ product.createdDate }}</td>
+      </tr>
+    </table>
+    <br>
+    <button
+      style="
+        background-color: white;
+        width: 100px;
+        height: 50px;
+        color: black;
+        border: solid 1px black;
+        margin-left: 100px;
+      "
+      @click="productAdd"
+    >
+      상품추가
+    </button>
+    <br>
+    <table id="customers">
+      <tr>
+        <th>취소</th>
+        <th>이미지</th>
+        <th>상품명</th>
+        <th>상품가격</th>
+        <th>재고</th>
+        <th>보증기간</th>
+        <th>게시일</th>
+      </tr>
+      <tr v-for="product in productCheckList" :key="i">
+        <td><input type="button" value="취소" style="background-color: wheat;"/></td>
+        <td>
+          <img :src="`${product.thumbnailUrl}`" style="height: 100px;"/>
+        </td>
+        <td>{{ product.productName }}</td>
+        <td>{{ product.price }}</td>
+        <td>{{ product.stock }}</td>
+        <td>{{ product.warranty }}</td>
+        <td>{{ product.createdDate }}</td>
+      </tr>
+    </table>
+    <button
+      style="background-color: black; width: 100px; height: 50px; color: white" @click="createCoupon"
+    >
+    등록
     </button>
   </main>
 </template>
@@ -168,10 +135,23 @@ export default {
       categoryList: [],
       categorySecondList: [],
       categoryThirdList: [],
-      firstNames: [],
-      secondNames: [],
-      thirdNames: [],
       productList: [],
+      firstClickValue:"",
+      secondClickValue:"",
+      thirdClickValue:"",
+      fistValue:"",
+      secondValue:"",
+      thirdValue:"",
+      productCheckVmodel:[],
+      productCheckList:[],
+      couponName:"",
+      couponContent:"",
+      discountRate:0.0,
+      startDate:"",
+      endDate:"",
+      minOrderPrice:0,
+      maxDiscountPrice:0,
+      productIdList:[]
     };
   },
   created() {
@@ -183,62 +163,49 @@ export default {
       console.log(this.categoryList);
     },
     changeCategoryFirst(event) {
-      this.categorySecondList =
-        this.categoryList[event.target.value].categorySecondDtoList;
-      let sel = document.getElementById('first_select');
-      // alert(sel.options[sel.selectedIndex].childNodes[1].id);
+      this.firstClickValue = event.target.value;
+      if (this.firstClickValue == -1){
+        this.firstValue=""
+      } else {
+        this.categorySecondList = this.categoryList[this.firstClickValue].categorySecondDtoList;
+        this.firstValue = this.categoryList[this.firstClickValue].id;
+      }
     },
     changeCategorySecond(event) {
-      this.categoryThirdList =
-        this.categorySecondList[event.target.value].categoryThirdDtoList;
-      let sel = document.getElementById('second_select');
-      // alert(sel.options[sel.selectedIndex].childNodes[1].id);
+      this.secondClickValue = event.target.value;
+      if (this.secondClickValue == -1){
+        this.secondValue=""
+      } else {
+        this.categoryThirdList = this.categorySecondList[this.secondClickValue].categoryThirdDtoList;
+        this.secondValue = this.categoryList[this.firstClickValue].categorySecondDtoList[this.secondClickValue].id;
+      }
     },
-    changeCategoryThird() {
-      let sel = document.getElementById('third_select');
-      // alert(sel.options[sel.selectedIndex].childNodes[1].id);
+    changeCategoryThird(event) {
+      this.thirdClickValue = event.target.value;
+      if (this.thirdClickValue == -1){
+        this.thirdValue=""
+      } else {
+        this.thirdValue = this.categoryList[this.firstClickValue].categorySecondDtoList[this.secondClickValue].categoryThirdDtoList[this.thirdClickValue].id;
+      }
     },
     calc() {
       if (document.querySelector('.myCheckbox').checked) {
         console.log(this.id);
       }
     },
-    testClick() {
-      let sel_1 = document.getElementById('first_select');
-      let fistValue = '';
-      console.log(sel_1.options[0]);
-      if (sel_1.options.value == undefined) {
-        this.fistValue = '';
-      } else {
-        this.fistValue = sel_1.options[sel_1.selectedIndex].childNodes[1].id;
-      }
-
-      let sel_2 = document.getElementById('second_select');
-      let secondValue = '';
-      if (sel_2.options.value == undefined) {
-        this.secondValue = '';
-      } else {
-        this.secondValue = sel_2.options[sel_2.selectedIndex].childNodes[1].id;
-      }
-
-      let sel_3 = document.getElementById('third_select');
-      let thirdValue = '';
-      if (sel_3.options.value == undefined) {
-        this.thirdValue = '';
-      } else {
-        this.thirdValue = sel_3.options[sel_3.selectedIndex].childNodes[1].id;
-      }
-
-      console.log('value', fistValue, secondValue, thirdValue);
-
+    productSearch() {
+      let firstClickId = parseInt(this.firstValue);
+      let secondClickId = parseInt(this.secondValue);
+      let thirdClickId = parseInt(this.thirdValue);
+      console.log(this.firstValue, this.secondValue, this.thirdValue);
       axios
         .post(
           'https://sbbro.xyz/api/product/sellers/products/category',
           {
-            stordId: 9,
-            categoryFirstId: fistValue,
-            categorySecondId: secondValue,
-            categoryThirdId: thirdValue,
+            stordId: localStorage.getItem('store_id'),
+            categoryFirstId: firstClickId,
+            categorySecondId: secondClickId,
+            categoryThirdId: thirdClickId,
           },
           {
             headers: {
@@ -252,6 +219,68 @@ export default {
           this.productList = response.data;
         });
     },
+    productAdd(){
+      let check=0;
+      this.productCheckVmodel.some((v) => {
+        this.productCheckList.some((w) => {
+          if (w.id == v.id){
+            alert("중복 상품이 포함되어있습니다.");
+            check=1;
+            return true;
+          }
+        })
+        if (check==1){
+          return true;
+        }
+      })
+      if (check==0){
+        this.productCheckVmodel.forEach((v) => {
+          this.productCheckList.push(v);
+        })
+      }
+
+      console.log(this.productCheckList);
+    },
+
+    createCoupon(){
+      this.productCheckList.forEach((v) => {
+        this.productIdList.push(v.id);
+      })
+      console.log(this.productIdList);
+      this.couponName = document.getElementById("couponName").value;
+      this.couponContent = document.getElementById("couponContent").value;
+      this.discountRate = document.getElementById("discountRate").value;
+      this.startDate = Date.parse(document.getElementById("startDate").value);
+      this.endDate = Date.parse(document.getElementById("endDate").value);
+      this.minOrderPrice = document.getElementById("minOrderPrice").value;
+      this.maxDiscountPrice = document.getElementById("maxDiscountPrice").value;
+      console.log(localStorage.getItem('store_id'));
+      axios
+        .post(
+          'https://sbbro.xyz/api/product/admin/coupons',
+          {
+            storeId: localStorage.getItem('store_id'),
+            name: this.couponName,
+            contents: this.couponContent,
+            startDate: new Date(this.startDate),
+            endDate: new Date(this.endDate),
+            discountRate: this.discountRate,
+            minOrderPrice: this.minOrderPrice,
+            maxDiscountPrice: this.maxDiscountPrice,
+            productId: this.productIdList
+          },
+          {
+            headers: {
+              Authorization: `Bearer ` + localStorage.getItem('token'),
+              'Content-Type': 'application/json',
+            },
+          },
+        )
+        .then(response => {
+          console.log(response.data);
+          this.productList = response.data;
+        });
+    }
   },
 };
 </script>
@@ -281,7 +310,6 @@ export default {
 #customers {
   font-family: Arial, Helvetica, sans-serif;
   border-collapse: collapse;
-  width: 50%;
 }
 
 #customers td,
